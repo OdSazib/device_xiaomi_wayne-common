39385668e87daa320683c3654daf3d731e357be3
OdSazib <odsazib@gmail.com>
2022-02-25 21:33:23 +0600

    wayne-common: Update path and dependency
    Change-Id: I35478de557e2c119ebff99796bbe6004907c0e2b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -26,7 +26,7 @@
 # Inherit from sdm660-common
 include device/xiaomi/sdm660-common/BoardConfigCommon.mk
 
-WAYNE_PATH := device/xiaomi/wayne-common
+WAYNE_COMMON_PATH := device/xiaomi/wayne-common
 
 # Crypto
 TARGET_HW_DISK_ENCRYPTION := true
@@ -35,16 +35,18 @@ TARGET_HW_DISK_ENCRYPTION := true
 TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 
 # Display
-TARGET_SCREEN_DENSITY := 420
+TARGET_SCREEN_DENSITY := 440
 
 # HIDL
-DEVICE_MANIFEST_FILE += $(WAYNE_PATH)/configs/vintf/manifest.xml
+DEVICE_MANIFEST_FILE += $(WAYNE_COMMON_PATH)/configs/vintf/manifest.xml
 
 # Kernel
-TARGET_KERNEL_CONFIG := wayne_defconfig
+TARGET_KERNEL_CONFIG := vendor/wayne_defconfig
+TARGET_KERNEL_SOURCE := kernel/msm-4.19
+TARGET_KERNEL_VERSION := 4.19
 
 # Vendor Security patch level
-VENDOR_SECURITY_PATCH := 2020-08-05
+VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # WLAN MAC
 WLAN_MAC_SYMLINK := true
